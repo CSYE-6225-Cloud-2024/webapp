a7723aef59cd721f64e0014180b1ad25cef75727
jay vekariya <vekariya.ja@northeastern.edu>
2024-02-02 22:21:23 -0500

    feat: added user model and sync with db
    added a user model using sequelize define and synced it with databas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import apiRouter from './routes/index'
+import { initDB } from './db'
 
 const app = express()
 
 app.use(express.json())
 app.use(apiRouter)
+initDB()
 
 export default app
--- src/util/db.ts
+++ src/util/db.ts
@@ -1,7 +1,10 @@
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
+import userInit from '../db/models/users.model'
 
 dotenv.config()
 const db_url = process.env.DATABASE_URL || ''
 
-export const db = new Sequelize(db_url)
+export const db: Sequelize = new Sequelize(db_url, {
+  logging: console.log,
+})
