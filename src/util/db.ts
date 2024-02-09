88f031d2ea1c4e6eb80e47a9eb9276c7ef1941a4
jay vekariya <vekariya.ja@northeastern.edu>
2024-02-09 14:17:20 -0500

    refactor: updated db connection file and git action
    updated db conncetion file to build the connection url from different env file and added postgres in the github action, as well as uncommented the test

--- src/util/db.ts
+++ src/util/db.ts
@@ -2,8 +2,13 @@ import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 import logger from './logger'
 
-dotenv.config()
-const db_url = process.env.DATABASE_URL || ''
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+const db_host = process.env.DB_HOST || ''
+const db_port = process.env.DB_PORT || ''
+const db_user = process.env.DB_USER || ''
+const db_password = process.env.DB_PASSWORD || ''
+const db_name = process.env.DB_NAME || ''
+const db_url = `postgres://${db_user}:${db_password}@${db_host}:${db_port}/${db_name}`
 
 export const db: Sequelize = new Sequelize(db_url, {
   dialect: 'postgres',
--- test/users.test.ts
+++ test/users.test.ts
@@ -1,69 +1,95 @@
-// import app from '../src'
-// import request from 'supertest'
-// import { users } from '../src/db/models/users.model'
+import app from '../src'
+import request from 'supertest'
+import { users } from '../src/db/models/users.model'
+import { db } from '../src/util/db'
 
 describe('users', () => {
-  test('should pass', () => {
-    expect(1).toBe(1)
+  beforeAll((done) => {
+    users.afterSync(async () => {
+      await users.destroy({ where: {} })
+    })
+    done()
+  })
+
+  afterAll(async () => {
+    await db.drop()
+  })
+
+  describe('create user and get that user', () => {
+    test('create user', async () => {
+      const response = await request(app).post('/v1/user').send({
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe@example.com',
+        password: 'password',
+      })
+      expect(response.status).toBe(201)
+      expect(response.body).toEqual({
+        id: expect.any(String),
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe@example.com',
+        account_created: expect.any(String),
+        account_updated: expect.any(String),
+      })
+    })
+
+    test('get user', async () => {
+      const response = await request(app)
+        .get('/v1/user/self')
+        .set(
+          'Authorization',
+          `Basic ${Buffer.from('johndoe@example.com:password').toString(
+            'base64'
+          )}`
+        )
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual({
+        id: expect.any(String),
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe@example.com',
+        account_created: expect.any(String),
+        account_updated: expect.any(String),
+      })
+    })
+  })
+
+  describe('update user and get with new password', () => {
+    test('update user', async () => {
+      const response = await request(app)
+        .put('/v1/user/self')
+        .set(
+          'Authorization',
+          `Basic ${Buffer.from('johndoe@example.com:password').toString(
+            'base64'
+          )}`
+        )
+        .send({
+          password: 'password2',
+        })
+      expect(response.status).toBe(204)
+      expect(response.body).toEqual({})
+    })
+
+    test('get user with new password', async () => {
+      const response = await request(app)
+        .get('/v1/user/self')
+        .set(
+          'Authorization',
+          `Basic ${Buffer.from('johndoe@example.com:password2').toString(
+            'base64'
+          )}`
+        )
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual({
+        id: expect.any(String),
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe@example.com',
+        account_created: expect.any(String),
+        account_updated: expect.any(String),
+      })
+    })
   })
 })
-
-// describe('users', () => {
-//   beforeAll((done) => {
-//     users.afterSync(async () => {
-//       await users.destroy({ where: {} })
-//     })
-//     done()
-//   })
-
-//   test('create user', async () => {
-//     const response = await request(app).post('/v1/user').send({
-//       first_name: 'John',
-//       last_name: 'Doe',
-//       username: 'johndoe@example.com',
-//       password: 'password',
-//     })
-//     expect(response.status).toBe(201)
-//   })
-
-//   test('get user', async () => {
-//     const response = await request(app)
-//       .get('/v1/user/self')
-//       .set(
-//         'Authorization',
-//         `Basic ${Buffer.from('johndoe@example.com:password').toString(
-//           'base64'
-//         )}`
-//       )
-//     expect(response.status).toBe(200)
-//   })
-
-//   describe('update user and get with new password', () => {
-//     test('update user', async () => {
-//       const response = await request(app)
-//         .put('/v1/user/self')
-//         .set(
-//           'Authorization',
-//           `Basic ${Buffer.from('johndoe@example.com:password').toString(
-//             'base64'
-//           )}`
-//         )
-//         .send({
-//           password: 'password2',
-//         })
-//       expect(response.status).toBe(204)
-//     })
-
-//     test('get user with new password', async () => {
-//       const response = await request(app)
-//         .get('/v1/user/self')
-//         .set(
-//           'Authorization',
-//           `Basic ${Buffer.from('johndoe@example.com:password2').toString(
-//             'base64'
-//           )}`
-//         )
-//       expect(response.status).toBe(200)
-//     })
-//   })
-// })
