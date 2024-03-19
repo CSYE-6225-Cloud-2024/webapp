0bbdebfd1decd54cb9aeeb06e3d0eb3f964ab7fd
jay vekariya <vekariya.ja@northeastern.edu>
2024-03-19 02:35:09 -0400

    fix: fixed the ops config location
    Changed the move location of the cloud ops agent config file to correct folder

--- packer/scripts/configOpsAgent.sh
+++ packer/scripts/configOpsAgent.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir /var/log/webapp
 
-sudo mv /tmp/config.yaml /var/log/webapp/config.yaml
+sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 
 sudo chown -R csye6225:csye6225 /var/log/webapp
 
